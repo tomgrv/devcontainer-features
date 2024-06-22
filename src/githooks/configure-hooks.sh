83ecdf091bc50d72de73dbb3b1145e99f26851ac
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-06-22 21:18:48 +0000

    fix: 🐛 update githooks and gitutils

--- src/githooks/configure-hooks.sh
+++ src/githooks/configure-hooks.sh
@@ -2,4 +2,12 @@
 set -e
 
 ### Define current script directory as hook directory
-git config hooks.hookDir $(dirname $(readlink -f $0))
+git config hooks.hookDir $source
+### Make a symbolic link to the hook directory for each hook starting with '_'  and ending with '.sh'
+find  $source -name '_*.sh' | sort | while read file; do
+    hook=$(basename $file | sed 's/^_//;s/\.sh$//')
+    echo "Link $file to .git/hooks/$hook" | npx chalk-cli --stdin yellow
+    ln -sf $file .git/hooks/$hook
+done
+
+
--- src/gitutils/install.sh
+++ src/gitutils/install.sh
@@ -3,25 +3,32 @@ set -e
 
 ### Init directories
 export source=$(dirname $(readlink -f $0))
-export feature=$(basename $source)
+export feature=$(basename $source | sed 's/_.*$//')
 export target=/usr/local/share/$feature
 echo "Activating feature <$feature>..."
+
+### Makes sure the target directory exists
 mkdir -p $target
 
-### For each json file starting with _ in the feature directory, copy it to the hooks directory
+### For each json file starting with _ in the feature directory, copy it to the target directory
 echo "Copying json files to $target..."
 find $source -type f -name "_*.json" | while read file; do
     cp $file $target/$(basename $file)
     echo "Copied $file => $target/$(basename $file)"
 done
 
-### Copy the config script to the hooks directory and create a git alias for it
-echo "Creating git alias for githooks configuration..."
+### For each sh scrrip starting with _ in the feature directory, copy it to the target directory
+echo "Copying scripts to $target..."
+find $source -type f -name "_*.sh" | while read script; do
+    name=$(basename $script)
+    cp $script $target/$name
+    chmod +x $target/$name
+    echo "Copied script $script => $target/$name"
+done
+
+### Copy the config script to the target directory and create a git alias for it
 cp $source/configure*.sh $target
 chmod +x $target/configure*.sh
-
-### Create git alias to configure hooks behaviors in current repository
-git config --system alias.init-${feature%_*} "!sh -c '$target/configure.sh' - "
 
 ### Call all the install-xxx scripts in the feature directory
 echo "Calling all install scripts in $source..."
