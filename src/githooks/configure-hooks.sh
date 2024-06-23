5ca1d8bb236885b6425610acbc6afcae6602cc47
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-06-23 20:53:02 +0000

    fix: 🐛 adjust cz calls

--- src/githooks/configure-hooks.sh
+++ src/githooks/configure-hooks.sh
@@ -2,7 +2,8 @@
 set -e
 
 ### Define current script directory as hook directory
-git config hooks.hookDir $source
+git config hooks.hooksPath $source
+
 ### Make a symbolic link to the hook directory for each hook starting with '_'  and ending with '.sh'
 find  $source -name '_*.sh' | sort | while read file; do
     hook=$(basename $file | sed 's/^_//;s/\.sh$//')
