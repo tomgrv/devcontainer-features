aa548179a055f32c9a373a689c58275a5d9d308f
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-06-27 12:36:20 +0000

    fix(deps): add docker dependency

--- src/act/install.sh
+++ src/act/install.sh
@@ -3,5 +3,18 @@ set -e
 
 echo "Activating feature 'nektos/act'"
 
+### Install Docker with apk on alpine and apt on ubuntu
+if [ -f /etc/alpine-release ]; then
+    echo "Installing Docker on Alpine"
+    apk add docker
+elif [ -f /etc/debian_version ]; then
+    echo "Installing Docker on Ubuntu"
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
 ### Install Act
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/${VERSION}/install.sh | sudo bash -s -- -b /usr/local/bin
