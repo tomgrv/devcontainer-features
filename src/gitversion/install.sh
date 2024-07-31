0949150b1ed078c60bc8a8836944802d4a43e0b5
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-07-31 12:38:02 +0000

    fix: 🐛 imporve scripts

--- src/gitversion/install.sh
+++ src/gitversion/install.sh
@@ -5,6 +5,4 @@ echo "Activating feature 'giversion ${VERSION}'"
 
 ### Install GitVersion
 dotnet tool install GitVersion.Tool --version ${VERSION} --tool-path /usr/local/bin
-sudo ln -s /usr/local/bin/dotnet-gitversion /usr/local/bin/gitversion
-
-
+ln -s /usr/local/bin/dotnet-gitversion /usr/local/bin/gitversion
