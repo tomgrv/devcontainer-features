75c0907348c0623ba8f7ee646c76b2b00e19f1ef
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-07-31 12:38:02 +0000

    fix: 🐛 imporve scripts

--- src/act/install.sh
+++ src/act/install.sh
@@ -4,4 +4,4 @@ set -e
 echo "Activating feature 'nektos/act'"
 
 ### Install Act
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/${VERSION}/install.sh | sudo bash -s -- -b /usr/local/bin
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/${VERSION}/install.sh | bash -s -- -b /usr/local/bin
