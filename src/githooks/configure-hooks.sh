5cc609f898fd91517dc7bbf98bf434b9730226f8
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-08-16 22:51:30 +0000

    fix: 🐛 fix hook path

--- src/githooks/configure-hooks.sh
+++ src/githooks/configure-hooks.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -e
 
-### Define current script directory as hook directory
-git config hooks.hooksPath $source
+### Define hook directory
+git config core.hooksPath .git/hooks
 
 ### Make a symbolic link to the hook directory for each hook starting with '_'  and ending with '.sh'
-find  $source -name '_*.sh' | sort | while read file; do
+find $source -name '_*.sh' | sort | while read file; do
     hook=$(basename $file | sed 's/^_//;s/\.sh$//')
     echo "Link $file to .git/hooks/$hook" | npx chalk-cli --stdin yellow
     ln -sf $file .git/hooks/$hook
 done
-
-
