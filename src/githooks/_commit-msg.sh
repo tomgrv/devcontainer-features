70d7c8a3aa77714a548a732e81274ee0105f7dca
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-08-17 18:37:57 +0000

    fix: 🐛 fix dependencies installation

--- src/githooks/_commit-msg.sh
+++ src/githooks/_commit-msg.sh
@@ -7,6 +7,6 @@ if [ -t 1 ]; then
 fi
 
 # Apply commitlint rules to the latest commit message
-npx chalk-cli --no-stdin -t "{blue →} Applying commitlint rules to the latest commit..."
+npx --yes chalk-cli --no-stdin -t "{blue →} Applying commitlint rules to the latest commit..."
 PLUGINS=$(cat package.json | npx jqn '.commitlint.extends' | tr -d "'[]:")
-npm list --global $PLUGINS 2>/dev/null 1>&2 || npm install --global $PLUGINS 2>/dev/null 1>&2 && npx commitlint --edit "$1" && npx devmoji -e
+npm list $PLUGINS 2>/dev/null 1>&2 || npm install --no-save $PLUGINS 2>/dev/null 1>&2 && npx --yes commitlint --edit "$1" && npx devmoji -e
