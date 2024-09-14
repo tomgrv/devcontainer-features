1092ae9737287639b33e5552ab68bd7398156e68
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-09-14 14:59:36 +0200

    fix: 🐛 fix typo

--- src/gitutils/_fixup.sh
+++ src/gitutils/_fixup.sh
@@ -12,6 +12,12 @@ cd "$(git rev-parse --show-toplevel)" >/dev/null
 #### INTEGRATE MODIFICATIONS
 git fetch --progress --prune --recurse-submodules=no origin >/dev/null
 
+#### LOAD PARAMETER
+if [ "$1" = '--help' ]; then
+	echo 'Usage: git fixup [--force] [<commit>]'
+	exit 0
+fi
+
 #### CHECK IF FIXUP COMMIT EXISTS
 echo 'Check if fixup commit exists...'
 if ! git isFixup; then
@@ -22,11 +28,15 @@ if ! git isFixup; then
 		exit 1
 	fi
 
-	#### LOOK FOR COMMIT TO FIXUP IF NOT GIVEN AS PARAMETER
-	if [ -z "$1" ]; then
+	#### LOOK FOR COMMIT TO FIXUP IF NOT GIVEN AS PARAMETER OR IF --force IS USED
+	if [ "$1" = '--force' ]; then
 		#### GET COMMIT TO FIXUP
-		echo 'Get commit to fixup...'
+		echo 'Get commit to fixup by rewriting history...'
 		git histo
+		read -p 'What commit to fix? ' sha
+	elif [ -z "$1" ]; then
+		echo 'Get commit to fixup without rewriting history...'
+		git fixable
 		read -p 'What commit to fix? ' sha
 	else
 		#### USE COMMIT TO FIXUP FROM PARAMETER
@@ -45,7 +55,10 @@ if ! git isFixup; then
 	#### START REBASE
 	git rebase -i --autosquash $sha~ --autostash --no-verify
 else
-	echo -e "\e[32mExisting !fixup commit found. Cannot proceed\e[0m"
+	echo -e "\e[32mExisting !fixup commit found. Continue rebasing...\e[0m"
+
+	#### STAGE CONFLICTED FILES AND CONTINUE REBASE
+	git add --update && git rebase --continue
 fi
 
 #### BACK
