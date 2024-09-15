30a41c77c89467092fa09d69247fff18a619133c
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-09-15 22:01:29 +0200

    fix: install error

--- src/pecl/install.sh
+++ src/pecl/install.sh
@@ -7,10 +7,11 @@ echo "Activating pecl extension '${EXTENSION}'"
 phpVersion=$(php -v | head -n 1 | awk '{print $2}')
 
 # Install pecl extension
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-	&& apt-get -y install --no-install-recommends lib${EXTENSION}-dev \
-	&& pecl install ${EXTENSION} \
-	&& mkdir -p /usr/local/php/$phpVersion/ini/conf.d \
-	&& echo "extension=${EXTENSION}" >> /usr/local/php/$phpVersion/ini/conf.d/${EXTENSION}.ini \
-	&& rm -rf /tmp/pear \
-	&& apt-get clean -y && rm -rf /var/lib/apt/lists/*
+apt-get update && export DEBIAN_FRONTEND=noninteractive &&
+	apt-get -y install --no-install-recommends lib${EXTENSION}-dev
+
+pecl install ${EXTENSION} &&
+	mkdir -p /usr/local/php/$phpVersion/ini/conf.d &&
+	echo "extension=${EXTENSION}" >>/usr/local/php/$phpVersion/ini/conf.d/${EXTENSION}.ini &&
+	rm -rf /tmp/pear &&
+	apt-get clean -y && rm -rf /var/lib/apt/lists/*
