ae4ee27059d18ec80750aad37db84ded20b284d0
Thomas G. <thomas.gervais@gmail.com>
2024-09-16 11:58:38 +0000

    refactor: ♻️ prevent npx validation issues

--- src/githooks/configure-hooks.sh
+++ src/githooks/configure-hooks.sh
@@ -7,6 +7,6 @@ git config core.hooksPath .git/hooks
 ### Make a symbolic link to the hook directory for each hook starting with '_'  and ending with '.sh'
 find $source -name '_*.sh' | sort | while read file; do
     hook=$(basename $file | sed 's/^_//;s/\.sh$//')
-    echo "Link $file to .git/hooks/$hook" | npx chalk-cli --stdin yellow
+    echo "Link $file to .git/hooks/$hook" | npx --yes chalk-cli --stdin yellow
     ln -sf $file .git/hooks/$hook
 done
--- src/gitutils/configure.sh
+++ src/gitutils/configure.sh
@@ -10,20 +10,20 @@ echo "Configuring feature <$feature>..."
 cd "$(git rev-parse --show-toplevel)" >/dev/null
 
 ### Create package.json if not exists or is empty
-echo "Merge all package folder json files into top level package.json" | npx chalk-cli --stdin blue
+echo "Merge all package folder json files into top level package.json" | npx --yes chalk-cli --stdin blue
 if [ ! -f package.json -o ! -s package.json ]; then
     echo "{}" >package.json
 fi
 
 ### Merge all package folder json files into top level package.json
 find $source -name _*.json | sort | while read file; do
-    echo "Merge $file" | npx chalk-cli --stdin yellow
+    echo "Merge $file" | npx --yes chalk-cli --stdin yellow
     jq -s '.[1] * .[0]' $file package.json >/tmp/package.json && mv -f /tmp/package.json package.json
 done
 
 ### Call all configure-xxx.sh scripts
 find $source -name configure-*.sh | sort | while read file; do
-    echo "Run $file" | npx chalk-cli --stdin yellow
+    echo "Run $file" | npx --yes chalk-cli --stdin yellow
     $file
 done
 
