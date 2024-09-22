5134a6058e08ee0d7864eadc39b33ec5df831906
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-09-22 17:59:48 +0000

    feat: 💥 ✨ Initiate larasets feature
    Migrate laravel settings to larasets
    
    BREAKING CHANGE: Migrate laravel settings to larasets

--- stubs/.devcontainer/create.sh
+++ stubs/.devcontainer/create.sh
@@ -3,17 +3,3 @@
 ### Ensure correct access rights
 sudo chown -Rf vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
-
-#### Create aliases
-sudo cat <<EOF > ~/.bash_aliases
-alias part='php artisan'
-alias sart='sail artisan'
-alias snpm='sail npm'
-alias snpx='sail npx --yes'
-alias spm2='sail npx --yes pm2'
-alias smix='spm2 restart MixWatch'
-alias smiw='spm2 --name MixWatch start npm -- run watch'
-alias gitv='gitversion'
-alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
-EOF
- 
