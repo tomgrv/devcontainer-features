307f84bed262d160bdfa9e5053373e96d399b37d
Thomas G. <tomgrv@users.perspikapps.fr>
2024-09-22 10:42:06 +0000

    feat: ✨ fix laravel config

--- stubs/.devcontainer/start.sh
+++ stubs/.devcontainer/start.sh
@@ -5,13 +5,24 @@ git config --global --add safe.directory ${containerWorkspaceFolder:-.}
 
 ### Install composer dependencies if composer.json exists
 if [ -f ${containerWorkspaceFolder:-.}/composer.json ]; then
-    composer install --ignore-platform-reqs --no-interaction --no-progress
+    composer install --ansi --ignore-platform-reqs --no-interaction --no-progress
 fi
 
 ### Install npm dependencies if package.json exists
 if [ -f ${containerWorkspaceFolder:-.}/package.json ]; then
     npm install --ws --if-present --include-workspace-root
 fi
+
+### Init db if sqlite
+if [ -f $DB_DATABASE ]; then
+    touch $DB_DATABASE
+fi
+
+### Init env
+touch ${containerWorkspaceFolder:-.}/.env
+
+### Add APP_KEY to .env if it does not exist, in one line
+grep -q "APP_KEY" ${containerWorkspaceFolder:-.}/.env || echo APP_KEY=null >> ${containerWorkspaceFolder:-.}/.env
 
 ### Init laravel
 if [ -f ${containerWorkspaceFolder:-.}/vendor/bin/sail ] && [ "${LARAVEL_SAIL}" -eq "1" ]; then
@@ -21,20 +32,19 @@ if [ -f ${containerWorkspaceFolder:-.}/vendor/bin/sail ] && [ "${LARAVEL_SAIL}" 
     ${containerWorkspaceFolder:-.}/vendor/bin/sail artisan config:cache
     ${containerWorkspaceFolder:-.}/vendor/bin/sail artisan view:cache
     ${containerWorkspaceFolder:-.}/vendor/bin/sail artisan route:cache 
-    ${containerWorkspaceFolder:-.}/vendor/bin/sail artisan migrate --seed
-    ${containerWorkspaceFolder:-.}/vendor/bin/sail npx --yes pm2 --name MixWatch start npm -- run watch
+    ${containerWorkspaceFolder:-.}/vendor/bin/sail artisan migrate --seed --graceful
+    ${containerWorkspaceFolder:-.}/vendor/bin/sail npx --yes pm2 --name devserver start npm -- run watch
 
 elif [ -f ${containerWorkspaceFolder:-.}/artisan ]; then
-    touch $DB_DATABASE
-    echo APP_KEY=null > .env
+
     php -d xdebug.mode=off artisan key:generate --force
     php -d xdebug.mode=off artisan config:cache
     php -d xdebug.mode=off artisan view:cache
     php -d xdebug.mode=off artisan route:cache
-    php -d xdebug.mode=off artisan migrate --seed
-    npx --yes pm2 --name MixWatch start npm -- run watch
+    php -d xdebug.mode=off artisan migrate --seed --graceful
+    npx --yes pm2 --name devserver start npm -- run watch
 fi
 
 ### Self update
 echo "Self updating devcontainer..."
-npx --yes $(sed -e 's://.*$::g' .devcontainer/devcontainer.json | npx --yes jqn '.name' | tr -d "'[]:,") 2> /dev/null 1>&2
+npx --yes $(sed -e 's://.*$::g' .devcontainer/devcontainer.json | npx --yes jqn '.name' | tr -d "'[]:,")  -a
