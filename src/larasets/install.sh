d149a95abacd9703982c2e2c50f17250e7c39ae6
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-09-22 21:35:18 +0000

    fix: 🐛 fix laraset configure missing

--- src/larasets/install.sh
+++ src/larasets/install.sh
@@ -7,17 +7,34 @@ export feature=$(basename $source | sed 's/_.*$//')
 export target=${1:-/usr/local/share}/$feature
 echo "Activating feature <$feature>..."
 
-#### Create aliases
-sudo cat <<EOF > ~/.bash_aliases
-alias art='php artisan'
-alias pm2='npx --yes pm2'
-alias dev='pm2 restart devserver || pm2 --name devserver start npm -- run dev'
-alias sart='sail artisan'
-alias snpm='sail npm'
-alias snpx='sail npx --yes'
-alias spm2='sail npx --yes pm2'
-alias sdev='spm2 restart devserver || spm2 --name devserver start npm -- run dev'
-alias gitv='gitversion'
-alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
-EOF
- 
+### Makes sure the target directory exists
+mkdir -p $target
+
+### For each json file starting with _ in the feature directory, copy it to the target directory
+echo "Copying json files to $target..."
+find $source -type f -name "_*.json" | while read file; do
+    name=$(basename $file)
+    cp $file $target/$name
+    echo "Copied $file => $target/$name"
+done
+
+### For each sh scrrip starting with _ in the feature directory, copy it to the target directory
+echo "Copying scripts to $target..."
+find $source -type f -name "_*.sh" | while read script; do
+    name=$(basename $script)
+    cp $script $target/$name
+    chmod +x $target/$name
+    echo "Copied script $script => $target/$name"
+done
+
+### Copy the config script to the target directory and create a git alias for it
+cp $source/configure*.sh $target
+chmod +x $target/configure*.sh
+
+### Call all the install-xxx scripts in the feature directory
+echo "Calling all install scripts in $source..."
+find $source -type f -name "install-*.sh" | while read script; do
+    echo "Calling $script..."
+    sh $script
+done
+
