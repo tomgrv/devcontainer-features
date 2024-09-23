d0299e10229ff4459fb6d272d81817be78414d9f
Thomas G. <tomgrv@users.perspikapps.fr>
2024-09-23 19:52:11 +0000

    refactor: ♻️ move stubs to features

--- src/gitutils/install.sh
+++ src/gitutils/install.sh
@@ -6,26 +6,8 @@ export source=$(dirname $(readlink -f $0))
 export feature=$(basename $source | sed 's/_.*$//')
 export target=${1:-/usr/local/share}/$feature
 echo "Activating feature <$feature>..."
-
 ### Makes sure the target directory exists
 mkdir -p $target
-
-### For each json file starting with _ in the feature directory, copy it to the target directory
-echo "Copying json files to $target..."
-find $source -type f -name "_*.json" | while read file; do
-    name=$(basename $file)
-    cp $file $target/$name
-    echo "Copied $file => $target/$name"
-done
-
-### For each sh scrrip starting with _ in the feature directory, copy it to the target directory
-echo "Copying scripts to $target..."
-find $source -type f -name "_*.sh" | while read script; do
-    name=$(basename $script)
-    cp $script $target/$name
-    chmod +x $target/$name
-    echo "Copied script $script => $target/$name"
-done
 
 ### Copy the config script to the target directory and create a git alias for it
 cp $source/configure*.sh $target
