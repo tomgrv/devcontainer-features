28070ab1c568b9b4af0bb2472cd905f322439db9
Thomas G. <tomgrv@users.perspikapps.fr>
2024-09-30 19:24:52 +0200

    fix: 🐛 fix git config

--- src/gitversion/install.sh
+++ src/gitversion/install.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
 set -e
 
+### Check if utils are installed
+utils() {
+    for bin in $* dos2unix jq; do
+        if [ -n "$(command -v $bin)" ]; then
+            echo "$bin is installed."
+        elif [ -f /etc/alpine-release ]; then
+            apk update
+            apk add $bin
+        elif [ $(uname) = "Linux" ] || [ $(uname) = "Darwin" ]; then
+            sudo apt-get update
+            sudo apt-get install -y $bin
+        else
+            echo "Please install $bin."
+            exit 1
+        fi
+    done
+} && utils
+
 ### Init directories
 export source=$(dirname $(readlink -f $0))
 export feature=$(basename $source | sed 's/_.*$//')
 export target=${1:-/usr/local/share}/$feature
 echo "Activating feature <$feature>..."
+
 ### Makes sure the target directory exists
 mkdir -p $target
 
 ### Copy the config script to the target directory and create a git alias for it
-cp $source/configure*.sh $target
-chmod +x $target/configure*.sh
+find $source -type f -name "configure*.sh" -exec cp {} $target \;
+find $target -type f -name "configure*.sh" -exec chmod +x {} \;
 
 ### Call all the install-xxx scripts in the feature directory
 echo "Calling all install scripts in $source..."
