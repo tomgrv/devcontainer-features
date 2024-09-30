cdecd0f522014a8a25fc9d155198bef095f7586c
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-09-30 19:24:52 +0200

    fix: 🐛 fix git config

--- src/larasets/configure-aliases.sh
+++ src/larasets/configure-aliases.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+#### Do not use sudo if on mingw
+if [ $(uname) = "Linux" ] || [ $(uname) = "Darwin" ]; then
+    SUDO=sudo
+else
+    SUDO=
+fi
+
 #### Create aliases
-sudo cat <<EOF > ~/.bash_aliases
+$SUDO cat <<EOF >~/.bash_aliases
 alias art='php artisan'
 alias pm2='npx --yes pm2'
 alias dev='pm2 restart devserver || pm2 --name devserver start npm -- run dev'
@@ -14,4 +21,3 @@ alias sdev='spm2 restart devserver || spm2 --name devserver start npm -- run dev
 alias gitv='gitversion'
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
 EOF
- 
