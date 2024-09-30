923d8943f286efe13a2420afb964a688f107427a
Thomas G. <tomgrv@users.perspikapps.fr>
2024-09-30 19:24:52 +0200

    fix: :bug: fix git config

---
+++ src/gitutils/install-config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+### For each entry in config.json file next to this file, create corresponding git config from key and value.
+### if value is an object, parse it as json and create dotted keys
+if [ -f "$source/config.json" ]; then
+    echo "Configuring git with <$source/config.json>..."
+    jq -r 'paths(scalars) as $p | [($p|join(".")), (getpath($p)|tostring)] | join(" ")' $source/config.json | while read key value; do
+        git config --system $key "$value"
+        echo "Created config $key => $value"
+    done
+fi
+
+### For each entry in alias.json file next to this file, create corresponding git alias from key and value
+if [ -f "$source/alias.json" ]; then
+    echo "Configuring aliases with <$source/alias.json>..."
+    jq -r 'keys[]' $source/alias.json | dos2unix | while read key; do
+        value=$(jq -r ".$key" $source/alias.json)
+        git config --system alias.$key "!sh -c '$value' - "
+        echo "Created alias $key => $value"
+    done
+fi
--- src/larasets/install.sh
+++ src/larasets/install.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+### Check if utils are installed
+utils() {
+    for bin in $* dos2unix jq; do
+        if [ -n "$(command -v $bin)" ]; then
+            echo "$bin is installed."
+        elif [ -f /etc/alpine-release ]; then
+            apk update
+            apk add $bin
+        elif [ $(uname) = "Linux" ] || [ $(uname) = "Darwin" ]; then
+            sudo apt-get update
+            sudo apt-get install -y $bin
+        else
+            echo "Please install $bin."
+            exit 1
+        fi
+    done
+} && utils
+
 ### Init directories
 export source=$(dirname $(readlink -f $0))
 export feature=$(basename $source | sed 's/_.*$//')
@@ -28,8 +46,8 @@ find $source -type f -name "_*.sh" | while read script; do
 done
 
 ### Copy the config script to the target directory and create a git alias for it
-cp $source/configure*.sh $target
-chmod +x $target/configure*.sh
+find $source -type f -name "configure*.sh" -exec cp {} $target \;
+find $target -type f -name "configure*.sh" -exec chmod +x {} \;
 
 ### Call all the install-xxx scripts in the feature directory
 echo "Calling all install scripts in $source..."
@@ -37,4 +55,3 @@ find $source -type f -name "install-*.sh" | while read script; do
     echo "Calling $script..."
     sh $script
 done
-
