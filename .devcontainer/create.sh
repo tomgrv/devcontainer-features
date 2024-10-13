0dd599f9f7faa0b4297e89ab873afe3908f0a8cf
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-10-13 21:36:01 +0000

    feat: ✨ improve update

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ### Ensure correct access rights
-sudo chown -Rf vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+sudo chown -Rf root ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
