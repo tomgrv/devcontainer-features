e5ab17387b7cff55a9e4d48f57e47929f0b31a07
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-11-24 15:38:43 +0000

    chore: 🔧 fix versions on working state

--- stubs/.devcontainer/create.sh
+++ stubs/.devcontainer/create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ### Ensure correct access rights
-sudo chown -Rf root ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+sudo chown -Rf $(id -un):$(id -gn) ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
