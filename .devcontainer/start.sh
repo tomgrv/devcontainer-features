0de2608b0b42ea0b88f1c02875921cc321b9a170
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-12-01 22:06:35 +0000

    fix: 🐛 fix tabsize on create, fix cz init

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,6 +3,11 @@
 ### Make sure the workspace folder is owned by the user
 git config --global --add safe.directory ${containerWorkspaceFolder:-.}
 
+### Define gpg configuration
+if [ -z "$CODESPACES" ]; then
+    git config --global gpg.program gpg2
+fi
+
 ### Self update
 echo "Self updating devcontainer..."
 npx --yes $(sed -e 's://.*$::g' .devcontainer/devcontainer.json | npx --yes jqn '.name' | tr -d "'[]:,") -u
