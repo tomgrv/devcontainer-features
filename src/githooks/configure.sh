4a3e355c2f83ef29b60ff673dc33cf0fd70a77e0
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-12-01 22:06:35 +0000

    fix: 🐛 fix tabsize on create, fix cz init

--- src/githooks/configure.sh
+++ src/githooks/configure.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+### Get indent size from devcontainer.json with jq, default to 2 if not found
+export tabSize=$(sed 's/\/\/.*//' .devcontainer/devcontainer.json | jq '.customizations.vscode.settings["editor.tabSize"] // 2')
+
 ### Init directories
 export source=$(dirname $(readlink -f $0))
 export feature=$(basename $source | sed 's/_.*$//')
@@ -22,13 +25,13 @@ fi
 ### Merge all package folder json files into top level package.json
 find $source -name _*.json | sort | while read file; do
     echo "Merge $file" | npx --yes chalk-cli --stdin yellow
-    jq -s '.[1] * .[0]' $file package.json >.package.json.$$ && mv -f .package.json.$$ package.json
+    jq --indent ${tabSize:-2} -s '.[0] * .[1]' $file package.json >/tmp/package.json && mv -f /tmp/package.json package.json
 done
 
 ### Call all configure-xxx.sh scripts
 find $source -name configure-*.sh | sort | while read file; do
     echo "Run $file" | npx --yes chalk-cli --stdin yellow
-    $file
+    $file || true
 done
 
 ### Sort package.json
--- src/larasets/configure.sh
+++ src/larasets/configure.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+### Get indent size from devcontainer.json with jq, default to 2 if not found
+export tabSize=$(sed 's/\/\/.*//' .devcontainer/devcontainer.json | jq '.customizations.vscode.settings["editor.tabSize"] // 2')
 
 ### Init directories
 export source=$(dirname $(readlink -f $0))
@@ -13,20 +15,24 @@ cd "$(git rev-parse --show-toplevel)" >/dev/null
 ### Create package.json if not exists or is empty
 echo "Merge all package folder json files into top level package.json" | npx --yes chalk-cli --stdin blue
 if [ ! -f package.json -o ! -s package.json ]; then
+    # Create empty package.json
     echo "{}" >package.json
+else
+    # Pre-sort
+    npx --yes sort-package-json
 fi
 
 ### Merge all package folder json files into top level package.json
 find $source -name _*.json | sort | while read file; do
     echo "Merge $file" | npx --yes chalk-cli --stdin yellow
-    jq -s '.[1] * .[0]' $file package.json >/tmp/package.json && mv -f /tmp/package.json package.json
+    jq --indent ${tabSize:-2} -s '.[0] * .[1]' $file package.json >/tmp/package.json && mv -f /tmp/package.json package.json
 done
 
 ### Call all configure-xxx.sh scripts
 find $source -name configure-*.sh | sort | while read file; do
     echo "Run $file" | npx --yes chalk-cli --stdin yellow
-    $file
+    $file || true
 done
 
 ### Sort package.json
-npx --yes sort-package-json package.json
+npx --yes sort-package-json
