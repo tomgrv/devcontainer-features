66563f0bf5b42b80854d5b39b5850fb3523babb8
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-12-04 19:52:18 +0000

    feat: ✨ improve laravel aliases & sail startup

--- src/larasets/configure-aliases.sh
+++ src/larasets/configure-aliases.sh
@@ -10,14 +10,51 @@ fi
 
 #### Create aliases
 $SUDO cat <<EOF >~/.bash_aliases
-alias art='php artisan'
-alias pm2='npx --yes pm2'
-alias dev='pm2 restart devserver || pm2 --name devserver start npm -- run dev'
-alias sart='sail artisan'
-alias snpm='sail npm'
-alias snpx='sail npx --yes'
-alias spm2='sail npx --yes pm2'
-alias sdev='spm2 restart devserver || spm2 --name devserver start npm -- run dev'
+
 alias gitv='gitversion'
-alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+alias sail='sh $([ -f sail ] && echo sail || echo \${containerWorkspaceFolder:-.}/vendor/bin/sail)'
+
+art() {
+    test -n "\$LARAVEL_SAIL" && test "\$LARAVEL_SAIL" -eq 1 && sail artisan "\$@" || php -d xdebug.mode=off \${CODESPACE_VSCODE_FOLDER:-.}/artisan "\$@"
+}
+
+export -f art
+
+srv()  {
+    test -n "\$LARAVEL_SAIL" && test "\$LARAVEL_SAIL" -eq 1 && sail npx --yes pm2 "\$@" || npx --yes pm2 "\$@"
+}
+
+export -f srv
+
+run()  {
+    srv restart server_\$1 || srv --name server_\$1 start npm -- run \$1
+}
+
+export -f run
+
+log()  {
+    srv logs server_\$1
+}
+
+export -f log
+
+refresh() {
+    art config:cache
+    art view:cache
+    art route:cache
+    art optimize:clear
+}
+
+export -f refresh
+
+init() {
+    test -f \${CODESPACE_VSCODE_FOLDER:-.}/artisan || return 1
+    test -n "\$LARAVEL_SAIL" && test "\$LARAVEL_SAIL" -eq 1 && sail up --build -d
+    art key:generate --force
+    refresh
+    art migrate --graceful --no-interaction
+    run dev
+}
+
+export -f init
 EOF
