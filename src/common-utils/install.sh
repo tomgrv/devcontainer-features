1b9a641193f67fcb7be3491140f6a0d96b63dfab
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2024-12-27 21:11:03 +0000

    feat: ✨ add common utils

---
+++ src/common-utils/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+### Options
+UTILS="${UTILS:-"jq dos2unix"}"
+
+### Install utils
+for bin in $UTILS; do
+	if [ -n "$(command -v $bin)" ]; then
+		echo "$bin is installed."
+	elif [ -f /etc/alpine-release ]; then
+		apk update
+		apk add $bin
+	elif [ $(uname) = "Linux" ] || [ $(uname) = "Darwin" ]; then
+		sudo apt-get update
+		sudo apt-get install -y $bin
+	else
+		echo "Please install $bin."
+		exit 1
+	fi
+done
+
+### Install this feature
+$(dirname $0)/_install-feature.sh
