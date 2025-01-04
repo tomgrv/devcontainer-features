6843823b165760088de3401f7579ba90406b92d0
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-04 14:07:11 +0100

    chore: 🔧 add comments

--- src/larasets/configure-sail.sh
+++ src/larasets/configure-sail.sh
@@ -8,3 +8,5 @@ init
 if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]; then
     sail build
 fi
+
+echo "Laravel Sail is ready to use. Run 'sail up' to start the containers." | npx --yes chalk-cli --stdin green
