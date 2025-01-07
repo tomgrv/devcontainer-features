07f40c250954ef7f461eb5370d7ee381ae9860a5
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-07 21:30:09 +0000

    feat: ✨ move update to tasks

--- stubs/.devcontainer/start.sh
+++ stubs/.devcontainer/start.sh
@@ -7,7 +7,3 @@ git config --global --add safe.directory ${containerWorkspaceFolder:-.}
 if [ -z "$CODESPACES" ]; then
     git config --global gpg.program gpg2
 fi
-
-### Self update
-echo "Self updating devcontainer..."
-npx --yes $(sed -e 's://.*$::g' ${containerWorkspaceFolder:-.}/.devcontainer/devcontainer.json | npx --yes jqn '.name' | tr -d "'[]:,") -u
