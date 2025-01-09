d2c7dab78f928c8ca8c4ef807aa5b6552ada0feb
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-09 21:27:23 +0000

    feat: ✨ pm2 logs handling

--- src/larasets/_srv.sh
+++ src/larasets/_srv.sh
@@ -2,11 +2,11 @@
 set -e
 
 workspace=${containerWorkspaceFolder:-${CODESPACE_VSCODE_FOLDER:-.}}
-if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]
-then
+if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]; then
     server='sail npx --yes pm2'
 else
     server='npx --yes pm2'
 fi
 
-$server restart server_\$1 || $server --name server_\$1 start npm -- run "\$@"
+$server restart server_$1 || $server --name server_$1 start "FORCE_COLOR=1 npm -- run \"$@\""
+$server log --raw --out server_$1
