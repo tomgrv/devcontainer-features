3cfe207f4f4436d044d2a47f44e385648d0b9d23
Thomas G. <tomgrv@users.perspikapps.fr>
2025-01-09 22:46:20 +0000

    fix: 🐛 scripting

--- src/larasets/_art.sh
+++ src/larasets/_art.sh
@@ -2,4 +2,8 @@
 set -e
 
 workspace=${containerWorkspaceFolder:-${CODESPACE_VSCODE_FOLDER:-.}}
-[ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ] && sail artisan "$@" || php -d xdebug.mode=off $workspace/artisan "$@"
+if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]; then
+    sail artisan "$@"
+else
+    php $workspace/artisan "$@"
+fi
