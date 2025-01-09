df584465d8295970171895034b24908c688a53cc
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-09 23:15:46 +0000

    fix: 🐛 tasks

--- src/larasets/_seed.sh
+++ src/larasets/_seed.sh
@@ -2,9 +2,9 @@
 set -e
 
 art key:generate --force
-art migrate --seed --graceful --no-interaction
 art config:cache
 art view:cache
 art route:cache
 art event:cache
-art optimize:clear
+art optimize
+art migrate --seed --graceful --no-interaction
