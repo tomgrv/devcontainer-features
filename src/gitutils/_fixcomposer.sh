e937aea18bfc06825a2a29b51eac07404173a40d
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-18 17:29:35 +0100

    feat: ✨ add git commands

---
+++ src/gitutils/_fixcomposer.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#### Go to root directory
+cd "$(git rev-parse --show-toplevel)"
+
+### Stash all changes including untracked files
+git stash -u
+
+#### Get commit to fixup
+sha=$(git getcommit "$@")
+
+#### Check if fixup commit is valid
+if [ -z "$sha" ]; then
+    echo "No commit to fixup"
+    exit 1
+fi
+
+#### Display commit to fixup
+echo "Fixing commit: $sha"
+
+### Redo all composer.lock files
+git filter-branch --tree-filter '
+    composer validate --no-check-all --strict 2>&1 | grep -oP "Required package \"\K[^\"]+" | while read -r package; do
+        composer require --ignore-platform-reqs --no-scripts --no-interaction --no-progress --no-install "$package"
+    done
+' $sha..HEAD
+
+### Unstash changes
+git stash pop
+
+#### Back to previous directory
+cd - >/dev/null
