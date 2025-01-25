f7597d3746d4e36b37a70982a85f6269527d4e31
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-01-25 10:40:02 +0100

    feat: ✨ handle sail not running

--- src/larasets/_run.sh
+++ src/larasets/_run.sh
@@ -2,8 +2,7 @@
 set -e
 
 workspace=${containerWorkspaceFolder:-${CODESPACE_VSCODE_FOLDER:-.}}
-if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]
-then
+if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ] && sail ps --status running | grep --after-context=1 -q -; then
     sail npm run "$@"
 else
     npm run "$@"
