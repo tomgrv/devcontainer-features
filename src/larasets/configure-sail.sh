e7896bc84dd55a9d6d8f20ed6fefbf5ad6a441c7
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-02-10 15:50:38 +0000

    fix: 🐛 remove redundant sorting for JSON files in package.json

--- src/larasets/configure-sail.sh
+++ src/larasets/configure-sail.sh
@@ -9,4 +9,4 @@ if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ]; then
     sail build
 fi
 
-echo "Laravel Sail is ready to use. Run 'sail up' to start the containers." | npx --yes chalk-cli --stdin green
+echo "Laravel Sail is ready to use. Run 'sail up' to start the containers."
