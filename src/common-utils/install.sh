0c26cd5a76ddb31fb337ab145cae87698116fab0
Thomas G. <tomgrv@users.perspikapps.fr>
2025-02-10 12:01:24 +0000

    fix: sh behavior

--- src/common-utils/install.sh
+++ src/common-utils/install.sh
@@ -24,6 +24,10 @@ for bin in $UTILS; do
 	fi
 done
 
+# Prepare for installation
+ln -sf _zz_args.sh zz_args
+ln -sf _zz_context.sh zz_context
+
 # Install the feature
 . $(dirname $0)/_install-bin.sh -s $PWD/$(dirname $0)
 . $(dirname $0)/_install-feature.sh -s $PWD/$(dirname $0)
