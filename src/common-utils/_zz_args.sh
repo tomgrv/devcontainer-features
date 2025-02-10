533df961fa40b28014f3b2b65471741ca9e3a2b2
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-02-10 15:50:38 +0000

    fix: 🐛 remove redundant sorting for JSON files in package.json

--- src/common-utils/_zz_args.sh
+++ src/common-utils/_zz_args.sh
@@ -1,111 +1,118 @@
 #!/bin/sh
 
-# Initialize variables
-count="0"
-value=""
-argname=""
-varname=""
-varnames=""
-argnames=""
-datatype=""
-help=""
-invert=""
+zz_args() {
 
-# Display help information if no arguments are passed
-if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "help" ]; then
-    echo "Usage: $(basename $0) <title> <caller> <<--help
+    # Initialize variables
+    local count="0"
+    local value=""
+    local argname=""
+    local varname=""
+    local varnames=""
+    local argnames=""
+    local datatype=""
+    local help=""
+    local invert=""
+
+    # Display help information if no arguments are passed
+    if test $# -lt 1; then
+        echo "Usage: $(basename $0) <title> <caller> <<--help
         ...
         <argname> <datatype> <varname> <help>
         ...
         help" >&2
-    exit 1
-fi
-
-# Set the title from the first argument
-title=$1 && shift
-
-# Set the caller from the second argument
-caller=$1 && shift
-
-# Read argument definitions from standard input
-while read argname datatype varname help; do
-
-    # Check if the argument requires a value
-    if [ "$datatype" = "-" ]; then
-        name=""
-    else
-        name="<$datatype>"
+        return 1
     fi
 
-    # Check if the argument is optional
-    if [ "$argname" = "-" ] || [ "$argname" = "+" ]; then
-        line="<$datatype>"
-        helpinfo="$helpinfo\n\t$(printf '%-12s : %s' "$name" "$help")"
-    else
+    # Set the title from the first argument
+    title=$1 && shift
+
+    # Set the caller from the second argument
+    caller=$1 && shift
+
+    echo "$title from $caller"
+
+    # Read argument definitions from standard input
+    while read argname datatype varname help; do
+
+        # Check if the argument requires a value
         if [ "$datatype" = "-" ]; then
-            line="[-$argname]"
-            argnames="$argnames$argname"
+            name=""
         else
-            line="[-$argname <$datatype>]"
-            argnames="$argnames$argname:"
+            name="<$datatype>"
         fi
 
-        helpinfo="$helpinfo\n\t$(printf '%-3s %-8s : %s' "-$argname" "$name" "$help")"
-    fi
+        # Check if the argument is optional
+        if [ "$argname" = "-" ] || [ "$argname" = "+" ]; then
+            line="<$datatype>"
+            helpinfo="$helpinfo\n\t$(printf '%-12s : %s' "$name" "$help")"
+        else
+            if [ "$datatype" = "-" ]; then
+                line="[-$argname]"
+                argnames="$argnames$argname"
+            else
+                line="[-$argname <$datatype>]"
+                argnames="$argnames$argname:"
+            fi
 
-    # Add the argument to the list of variables
-    varnames="$varnames\n$argname\t$varname"
-    lineinfo="$lineinfo $line"
+            helpinfo="$helpinfo\n\t$(printf '%-3s %-8s : %s' "-$argname" "$name" "$help")"
+        fi
 
-    [ "$argname" = "+" ] && break
-done
+        # Add the argument to the list of variables
+        varnames="$varnames\n$argname\t$varname"
+        lineinfo="$lineinfo $line"
 
-# Parse the command-line arguments
-while getopts :$argnames value "$@"; do
-    if [ "$value" = "?" ]; then
-        break
-    fi
+        [ "$argname" = "+" ] && break
+    done
 
-    naming=$(echo -e "$varnames" | grep -E "^$value" | cut -f2)
+    # Parse the command-line arguments
+    while getopts :$argnames value "$@"; do
+        if [ "$value" = "?" ]; then
+            break
+        fi
 
-    if [ -n "$OPTARG" ]; then
-        export "$naming=$OPTARG"
+        naming=$(echo -e "$varnames" | grep -E "^$value" | cut -f2)
+
+        if [ -n "$OPTARG" ]; then
+            export "$naming=$OPTARG"
+        else
+            export "$naming=-$value"
+        fi
+    done
+
+    # Display help information if requested
+    if [ "$OPTARG" = "h" ] || [ "$OPTARG" = "help" ]; then
+
+        (
+            echo -e ""
+            echo -e "$title"
+            echo -e ""
+            echo -e "Usage: $(basename $caller)$lineinfo; use -h for more information."
+            echo -e "$helpinfo"
+        ) >&2
+
+        exit 1
+
+    elif [ "$OPTARG" = "@" ]; then
+        echo "Stop processing arguments !" >&2
     else
-        export "$naming=-$value"
+        # Shift the processed arguments
+        shift $(expr "$OPTIND" - 1)
+
+        # Process remaining '-' parameters
+        for arg in $(echo -e $varnames | grep -E "^-" | cut -f2); do
+            if [ "$#" -gt "0" ]; then
+                export "$arg=$1" && shift 1
+            fi
+        done
+
+        # Process remaining '+' parameters
+        for arg in $(echo -e $varnames | grep -E "^\+" | cut -f2); do
+            if [ "$#" -gt "0" ]; then
+                export "$arg=$(echo $@ | sed "s/ /\\\\ /g")" && shift $#
+            fi
+        done
+
     fi
-done
+}
 
-# Display help information if requested
-if [ "$OPTARG" = "h" ] || [ "$OPTARG" = "help" ]; then
-
-    (
-        echo -e ""
-        echo -e "$title"
-        echo -e ""
-        echo -e "Usage: $(basename $caller)$lineinfo; use -h for more information."
-        echo -e "$helpinfo"
-    ) >&2
-
-    exit 1
-
-elif [ "$OPTARG" = "@" ]; then
-    echo "Stop processing arguments !" >&2
-else
-    # Shift the processed arguments
-    shift $(expr "$OPTIND" - 1)
-
-    # Process remaining '-' parameters
-    for arg in $(echo -e $varnames | grep -E "^-" | cut -f2); do
-        if [ "$#" -gt "0" ]; then
-            export "$arg=$1" && shift 1
-        fi
-    done
-
-    # Process remaining '+' parameters
-    for arg in $(echo -e $varnames | grep -E "^\+" | cut -f2); do
-        if [ "$#" -gt "0" ]; then
-            export "$arg=$(echo $@ | sed "s/ /\\\\ /g")" && shift $#
-        fi
-    done
-
-fi
+[ "${0##*/}" = "zz_args" ] && zz_args "$@"
