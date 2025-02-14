8de1e66494f5cc24de165f9e36173ab4630bfd5b
Thomas G. <tomgrv@users.perspikapps.fr>
2025-02-14 20:00:51 +0000

    fix: 🐛 update feature installation scripts and configurations

--- src/common-utils/_install-feature.sh
+++ src/common-utils/_install-feature.sh
@@ -15,8 +15,16 @@ fi
 
 echo "Installing feature <${Purple}$feature${None}>..."
 
+# Copy stubs to the target directory
+if [ -d $source/stubs ]; then
+    echo "${Blue}Copying stubs to '$target'...${None}"
+    mkdir -p $target/stubs && cp -r $source/stubs/* $target/stubs
+else
+    echo "${Yellow}No stubs found in '$source'${None}"
+fi
+
 # Install specific utils by copying them to the target directory and making them executable
-find $source \( -name "_*" -o -name "configure-*.sh" \) -type f -exec cp {} $target \;
+find $source \( -name "_*" -o -name "configure-*.sh" -o -path "stubs" \) -type f -exec cp {} $target \;
 find $target -type f -name "*.sh" -exec chmod +x {} \;
 
 # Call all the install-xxx scripts in the feature directory
