9188aca111811591d9c7d4542972bc7178a4c63f
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-03-22 14:17:25 +0000

    fix: 🐛 revert to /bin/sh

--- src/common-utils/_configure-feature.sh
+++ src/common-utils/_configure-feature.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Source colors script
 . zz_colors
@@ -12,7 +12,7 @@ help
 )
 
 if [ -z "$feature" ]; then
-    echo -e "Usage: configure-feature <feature>\r"
+    echo "Usage: configure-feature <feature>${End}"
     exit 1
 fi
 
@@ -22,8 +22,8 @@ export source=${source:-/usr/local/share/$feature}
 # Get the indent size from devcontainer.json with jq, default to 2 if not found
 export tabSize=4
 
-echo -e "Configuring feature <${Purple}$feature${None}>\r"
-echo -e "from <$source>\r"
+echo "Configuring feature <${Purple}$feature${None}>${End}"
+echo "from <$source>${End}"
 
 # Go to the module root
 cd "$(git rev-parse --show-toplevel)" >/dev/null
@@ -31,7 +31,7 @@ cd "$(git rev-parse --show-toplevel)" >/dev/null
 # Deploy stubs if existing
 if [ -d $source/stubs ]; then
 
-    echo -e "${Blue}Deploy stubs${End}"
+    echo "${Blue}Deploy stubs${End}"
 
     find $source/stubs -type f -name ".*" -o -type f | while read file; do
 
@@ -50,7 +50,7 @@ if [ -d $source/stubs ]; then
             # Remove # occurrences in the file path
             dest=$(echo $dest | sed 's/\/\#/\//g')
 
-            echo -e "Add '$dest' to .gitignore\r"
+            echo "Add '$dest' to .gitignore${End}"
 
             # Add to .gitignore if not already there
             grep -qxF $dest .gitignore || echo -e "$dest" >>.gitignore
@@ -67,7 +67,7 @@ if [ -d $source/stubs ]; then
 fi
 
 # Log the merging process
-echo -e "${Blue}Merge all package folder json files into top level xxx.json${End}"
+echo "${Blue}Merge all package folder json files into top level xxx.json${End}"
 
 for package in package composer; do
 
@@ -77,13 +77,13 @@ for package in package composer; do
         echo "{}" >$package.json
     else
         # Pre-sort the existing package.json
-        echo -e "${Yellow}Pre-merge normalize $package.json${End}"
+        echo "${Yellow}Pre-merge normalize $package.json${End}"
         normalize-json -s -a -i -t ${tabSize:-4} $package.json
     fi
 
     # Merge all package folder json files into the top-level package.json
     find $source -maxdepth 1 -name _*.$package.json | sort | while read file; do
-        echo -e "${Yellow}Merge $file in $package.json${End}"
+        echo "${Yellow}Merge $file in $package.json${End}"
         jq --indent ${tabSize:-4} -s '.[0] * .[1]' $file $package.json >/tmp/$package.json && mv -f /tmp/$package.json $package.json
     done
 
@@ -91,5 +91,6 @@ done
 
 # Call all configure-xxx.sh scripts
 find $source -maxdepth 1 -name configure-*.sh | sort | while read file; do
-    echo -e "${Yellow}Run $file${End}"
+    echo "${Yellow}Run $file${End}"
+    $file
 done
