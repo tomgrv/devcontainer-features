d60c7dc322b4f7ce91040f65dba0c738880782a4
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-03-22 12:40:06 +0000

    fix: 🐛 correct crlf

--- src/common-utils/_install-feature.sh
+++ src/common-utils/_install-feature.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Source colors script
 . zz_colors
@@ -9,18 +9,18 @@ eval $(
 )
 
 if [ -z "$feature" ]; then
-    echo "Usage: install-feature <feature>"
+    echo -e "Usage: install-feature <feature>\r"
     exit 1
 fi
 
-echo "Installing feature <${Purple}$feature${None}>..."
+echo -e "Installing feature <${Purple}$feature${None}>...\r"
 
 # Copy stubs to the target directory
 if [ -d $source/stubs ]; then
-    echo "${Blue}Copying stubs to '$target'...${None}"
+    echo -e "${Blue}Copying stubs to '$target'...${End}"
     mkdir -p $target/stubs && cp -r $source/stubs/* $target/stubs
 else
-    echo "${Yellow}No stubs found in '$source'${None}"
+    echo -e "${Yellow}No stubs found in '$source'${End}"
 fi
 
 # Install specific utils by copying them to the target directory and making them executable
@@ -28,9 +28,9 @@ find $source \( -name "_*" -o -name "configure-*.sh" -o -path "stubs" \) -type f
 find $target -type f -name "*.sh" -exec chmod +x {} \;
 
 # Call all the install-xxx scripts in the feature directory
-echo "${Blue}Calling all install scripts in '$source'...${None}"
+echo -e "${Blue}Calling all install scripts in '$source'...${End}"
 find $source -type f -name "install-*.sh" | while read script; do
-    echo "${Yellow}Calling $script...${None}"
+    echo -e "${Yellow}Calling $script...${End}"
     sh $script "$@"
-    echo "${Green}Done!${None}"
+    echo -e "${Green}Done!${End}"
 done
