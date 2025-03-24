ad2c717e02b66b4fe9e31baed454ff258b8cadd6
Thomas G. <tomgrv@users.perspikapps.fr>
2025-03-24 14:43:13 +0000

    feat: ✨ sort keys not defined in schema

--- src/common-utils/_normalize-json.sh
+++ src/common-utils/_normalize-json.sh
@@ -7,7 +7,7 @@ set -e
 
 # Function to print help and manage arguments
 eval $(
-    zz_args "Normalize JSON according to schema" $0 "$@" <<-help
+    zz_args "Normalize JSON according to schema" $0 "$@" <<- help
         w -         write	    write normalized json to original file
         t tabSize   tabSize	    tab size for indentation
         c -         cache	    allow caching of schema validation map
@@ -39,31 +39,37 @@ for file in $files; do
                 | map(select(length > 0))
                 | map(
                     split("\".\"") 
-                    | map(gsub("^\"|\"$"; "") | select(length > 0))                   
+                    | map(gsub("^\"|\"$"; "") | select(length > 0))
                 );
         def xpath($ary):
             . as $in
             | if ($ary|length) == 0 then null
                 else $ary[0] as $k
-                | if $k == []
-                then range(0;length) as $i | $in[$i] | xpath($ary[1:]) | [$i] + .
-                else .[$k] | xpath($ary[1:]) | [$k] + . 
-                end
-                end ;
+                    | if $k == []
+                        then range(0;length) as $i | $in[$i] | xpath($ary[1:]) | [$i] + .
+                        else .[$k] | xpath($ary[1:]) | [$k] + . 
+                        end
+                end;
         def paths($ary): $ary[] as $path | xpath($path);
         def traverse($paths): 
             . as $in
             | reduce paths($paths) as $p 
-                (null; setpath($p; $in | getpath($p)));
-        
-        traverse(transform($list))' >/tmp/$$.json
+                (null; setpath($p; $in 
+                            | getpath($p)
+                            | if type == "object" then with_entries(.key |= .)
+                            | to_entries
+                            | sort_by(.key)
+                            | from_entries else . end
+                        )
+                );
+        traverse(transform($list))' > /tmp/$$.json
 
     # Handle output
     if test -s /tmp/$$.json; then
         if test -z "$write"; then
             jq -C --indent ${tabSize:-2} . /tmp/$$.json
         else
-            jq -M --indent ${tabSize:-4} . /tmp/$$.json >$file
+            jq -M --indent ${tabSize:-4} . /tmp/$$.json > $file
         fi
         zz_log s "File {U $file} normalized"
     else
