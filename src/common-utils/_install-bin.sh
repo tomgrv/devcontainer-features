926f8b7ff2762c13e4e0c3b7e141200c994c5da3
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-03-25 15:08:25 +0000

    chore: 🔧 improve logging

--- src/common-utils/_install-bin.sh
+++ src/common-utils/_install-bin.sh
@@ -13,12 +13,11 @@ if [ -z "$feature" ]; then
     exit 1
 fi
 
-echo "Installing bin scripts for <${Purple}$feature${None}>...${End}"
+zz_log i "Installing bin scripts for {Purple $feature}..."
 
 # Find all shell scripts in the target directory, make them executable, and create symbolic links in /usr/local/bin
 find $target -type f -name "_*.sh" -exec echo {} \; -exec chmod +x {} \; | while read file; do
     # Create a symbolic link in /usr/local/bin with the script name (without the leading underscore and .sh extension)
     link=/usr/local/bin/$(basename $file | sed 's/^_//;s/.sh$//')
-    ln -sf $file $link
-    echo "${Green}Linked '$file' to '$link'${End}"
+    ln -sf $file $link && zz_log s "Linked {U $file} to {U $link}"
 done
--- src/githooks/configure-hooks.sh
+++ src/githooks/configure-hooks.sh
@@ -6,6 +6,5 @@ git config core.hooksPath .git/hooks
 ### Make a symbolic link to the hook directory for each hook starting with '_'  and ending with '.sh'
 find $source -name '_*.sh' | sort | while read file; do
     hook=$(basename $file | sed 's/^_//;s/\.sh$//')
-    zz_log - "Link $file to .git/hooks/$hook"
-    ln -sf $file .git/hooks/$hook
+    ln -sf $file .git/hooks/$hook && zz_log s "Linked {U $file} to {U .git/hooks/$hook}"
 done
