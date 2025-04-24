2d82614d94490957ef1bf2d9b21a90111e33814b
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-04-24 11:40:13 +0000

    fix: 🐛 update scripts & tasks

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -3,3 +3,14 @@
 ### Ensure correct access rights
 sudo chown -Rf vscode:vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+
+### Ensure SSH keys are available
+for f in known_hosts id_rsa; do
+    if [ -f "/home/root/.ssh/$f" ]; then
+        sudo cp /home/root/.ssh/$f /home/vscode/.ssh/$f
+        sudo chown vscode:vscode /home/vscode/.ssh/$f
+        sudo chmod 600 /home/vscode/.ssh/$f
+    fi
+done
+
+ssh-keyscan github.com >>/home/vscode/.ssh/known_hosts
