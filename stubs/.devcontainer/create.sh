91bcd2773f0a6ec928ad8189ab1c41614b159ce4
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-04-24 11:40:13 +0000

    fix: 🐛 update scripts & tasks

--- stubs/.devcontainer/create.sh
+++ stubs/.devcontainer/create.sh
@@ -3,3 +3,6 @@
 ### Ensure correct access rights
 sudo chown -Rf vscode:vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+
+### Add ssh-keyscan to known_hosts if 
+ssh-keyscan github.com >>/home/vscode/.ssh/known_hosts || true
