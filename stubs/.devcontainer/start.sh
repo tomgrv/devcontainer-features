a2c57b52d1f3433d88ceebc89075868e79326708
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-04-26 10:08:41 +0000

    feat: ✨ add secrets mgmt

--- stubs/.devcontainer/start.sh
+++ stubs/.devcontainer/start.sh
@@ -7,3 +7,5 @@ git config --global --add safe.directory ${containerWorkspaceFolder:-.}
 if [ -z "$CODESPACES" ]; then
     git config --global gpg.program gpg2
 fi
+
+echo "SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa | tr -d '\n')" >>.github/workflows/.secrets
