bfb840fdf3a6e643fc8c560ea6b8fb4ea8d2a803
tomgrv <thomas.gervais@gmail.com>
2025-04-30 13:20:19 +0000

    fix: 🐛 create script

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
+### Add ssh-keyscan to known_hosts
+mkdir -p /home/vscode/.ssh
+chmod 700 /home/vscode/.ssh
+ssh-keyscan github.com >>/home/vscode/.ssh/known_hosts
+
 ### Ensure correct access rights
 sudo chown -Rf vscode:vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
 sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
-
-### Ensure SSH keys are available
-for f in known_hosts id_rsa; do
-    if [ -f "/home/root/.ssh/$f" ]; then
-        sudo cp /home/root/.ssh/$f /home/vscode/.ssh/$f
-        sudo chown vscode:vscode /home/vscode/.ssh/$f
-        sudo chmod 600 /home/vscode/.ssh/$f
-    fi
-done
-
-### Add ssh-keyscan to known_hosts
-sudo mkdir -p /home/vscode/.ssh
-sudo chmod 700 /home/vscode/.ssh
-ssh-keyscan github.com >>/home/vscode/.ssh/known_hosts
