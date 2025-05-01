950a82ad113198e22bed32531549a8bf63b94741
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-05-01 16:50:11 +0000

    feat: ✨ update gitutils version and add VSCode tasks

--- stubs/.devcontainer/start.sh
+++ stubs/.devcontainer/start.sh
@@ -8,4 +8,5 @@ if [ -z "$CODESPACES" ]; then
     git config --global gpg.program gpg2
 fi
 
+### Set up SSH keys
 echo "SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa | tr -d '\n')" >>.github/workflows/.secrets
