e2ce012445fdacdc792ab9226244f4558408d83a
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-05-01 12:54:55 +0000

    chore: 🔧 refactor gitutils

--- src/gitutils/_release-prod.sh
+++ src/gitutils/_release-prod.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+set -e
+
+# Source colors script
+. zz_colors
+
+# Function to print help and manage arguments
+eval $(
+    zz_args "Release production branch" $0 "$@" <<-help
+	help
+)
+
 #### Goto repository root
 cd "$(git rev-parse --show-toplevel)" >/dev/null
 
@@ -7,49 +18,53 @@ cd "$(git rev-parse --show-toplevel)" >/dev/null
 if [ -n "$(git branch --list hotfix/*)" ]; then
     flow=hotfix
     name=$(git branch --list hotfix/* | sed 's/.*hotfix\///')
-    npx --yes chalk-cli -t "{green ✔} Hotfix branch found: {yellow $name}"
+    zz_log i "Hotfix branch found: {Yellow $name}"
 fi
 
 #### CHECK IF ON A RELEASE BRANCH, EXTRACT BRANCH NAME
 if [ -n "$(git branch --list release/*)" ]; then
     flow=release
     name=$(git branch --list release/* | sed 's/.*release\///')
-    npx --yes chalk-cli -t "{green ✔} Release branch found: {blue $name}"
+    zz_log i "Release branch found: {Blue $name}"
 fi
 
 #### CHECK IF A FLOW BRANCH IS FOUND
 if [ -z "$flow" ] && [ -f .git/RELEASE ]; then
     flow=release
     name=$(cat .git/RELEASE)
-
-    npx --yes chalk-cli -t "{green ✔} Release branch found: {blue $name}"
+    zz_log i "Release branch found: {Blue $name}"
     git checkout $flow/$name
 fi
 
 #### EXIT IF NO FLOW BRANCH IS FOUND
 if [ -z "$flow" ] || [ -z "$name" ]; then
-    npx --yes chalk-cli -t "{red ✘} No flow branch found"
+    zz_log e "No flow branch found"
     exit 1
 fi
 
 #### GET BUMP VERSION
 GBV=$(gitversion -config .gitversion -showvariable MajorMinorPatch)
-npx --yes chalk-cli -t "{green ✔} Bump version: {green $GBV}"
+if [ -z "$GBV" ]; then
+    zz_log e "Cannot get version from .gitversion"
+    exit 1
+fi
+
+zz_log i "Bump version: {Blue $GBV}"
 
 #### PREVENT GIT EDITOR PROMPT
 GIT_EDITOR=:
 
 #### UPDATE VERSION & CHANGELOG & FINISH RELEASE
-if npx --yes json --validate -q -f package.json && npx --yes commit-and-tag-version --skip.tag --no-verify --release-as $GBV; then
+if npx --yes commit-and-tag-version --skip.tag --no-verify --release-as $GBV; then
     if git flow $flow finish $name --tagname $GBV --message $GBV --push; then
-        npx --yes chalk-cli -t "{green ✔} Release finished: {blue $name -> $GBV}"
+        zz_log s "Release finished: {B $GBV}"
         rm -f .git/RELEASE
     else
         git undo
-        npx --yes chalk-cli -t "{red ✘} Cannot finish release. CHANGELOG & VERSION are not updated."
+        zz_log e "Cannot finish release. CHANGELOG & VERSION are not updated."
     fi
 else
-    npx --yes chalk-cli -t "{red ✘} Cannot update version & finish release"
+    zz_log e "Cannot update version & finish release"
 fi
 
 #### BACK
