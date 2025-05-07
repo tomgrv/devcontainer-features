98a24c69813ae97ae70431173ea936d344a83db3
tomgrv <thomas.gervais@gmail.com>
2025-05-07 12:29:22 +0000

    chore: 🔧 reorganize init/seed scripts

--- src/larasets/_seed.sh
+++ src/larasets/_seed.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 set -e
 
-art key:generate --force
-art config:cache
-art view:cache
-art route:cache
-art event:cache
-art optimize
+### Default DB_CONNECTION
+if [ -z "$DB_CONNECTION" ]; then
+    export DB_CONNECTION=sqlite
+fi
+
+### Init db if sqlite and not exists
+case $DB_CONNECTION in
+sqlite)
+    zz_log i "DB_CONNECTION is {Purple $DB_CONNECTION}"
+    ### Set default sqlite db
+    if [ -z "$DB_DATABASE" ]; then
+        export DB_DATABASE=database/database.sqlite
+    fi
+
+    zz_log i "Ensure sqlite db {Purple $DB_DATABASE} exist"
+    touch ./$DB_DATABASE
+    ;;
+*)
+    zz_log w "DB_CONNECTION {Purple $DB_CONNECTION} is not supported yet"
+    exit 1
+    ;;
+esac
+
+### Run migrations
 art migrate --seed --graceful --no-interaction
