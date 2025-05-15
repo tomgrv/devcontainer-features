36a851ea4828562edf3bd7546c7657f2a5771a8f
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-05-15 17:37:21 +0000

    chore: 🔧 refactor composer dependency handling

---
+++ src/larasets/configure-composer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+#### Goto repository root
+cd "$(git rev-parse --show-toplevel)" >/dev/null
+
+### Install composer dependencies if composer.json exists
+if [ -f "./composer.json" ]; then
+
+    opts="--prefer-stable --prefer-lowest --minimal-changes --with-all-dependencies --ignore-platform-reqs --dev --no-update"
+
+    zz_log i "Make sure common dependencies are declared"
+    jq '.dependencies | .[]' ${source:-.}/_require.json | xargs -I {} composer require $opts {}
+    jq '.devDependencies| .[]' ${source:-.}/_require.json | xargs -I {} composer require $opts {}
+
+fi
