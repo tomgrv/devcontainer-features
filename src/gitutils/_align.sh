685ecb02a4948e6e509cf2e405270aca29f4ff37
tomgrv <thomas.gervais@gmail.com>
2025-05-18 14:04:10 +0000

    feat: ✨ improve git align

--- src/gitutils/_align.sh
+++ src/gitutils/_align.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-## get the branch name whatever the form is
+# get the branch name whatever the form is
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-## get the remote branch name
+# get the remote branch name
 origin=$(git config --get branch.$branch.remote)
 
-git fetch $origin && git branch -m $branch $branch-to-delete && git checkout -b $branch $origin/$branch && git branch -D $branch-to-delete
+# Stash all changes, including untracked files, with a message
+git stash push -u -m "Stashing changes before aligning branch"
+
+# Fetch the latest changes from the remote and align the branch
+git fetch $origin
+git branch -m $branch $branch-to-delete
+if git checkout -b $branch $origin/$branch; then
+    git branch -D $branch-to-delete
+else
+    zz_log e "Failed to checkout branch $branch from $origin/$branch"
+    git branch -m $branch-to-delete $branch
+fi
+# Apply the stashed changes
+git stash pop
+
+# Print the current branch name
+zz_log i "Current branch: $(git rev-parse --abbrev-ref HEAD)"
