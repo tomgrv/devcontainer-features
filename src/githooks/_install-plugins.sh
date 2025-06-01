7c471e4feef0a8e58694ff2ef197e9f917fecc4d
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-06-01 20:28:27 +0000

    feat: ✨ improve plugin install

---
+++ src/githooks/_install-plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+json_key=$1
+json_file=${2:-package.json}
+
+plugins=$(cat $json_file | npx --yes jqn "$1" | tr -d "'[]:,\"\n")
+
+if [ -z "$plugins" ]; then
+    zz_log w "No plugins found at key {B $json_key} in {U $json_file}"
+elif npm list $plugins 2>/dev/null 1>&2; then
+    zz_log s "Plugins {B $plugins} already installed!"
+else
+    zz_log i "Installing plugins {B $plugins} ..."
+    npm install --no-save $plugins 2>/dev/null 1>&2
+    zz_log s "Plugins {B $plugins} installed successfully!"
+fi
