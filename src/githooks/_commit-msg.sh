de1d5994ba75dc02ba2680143d5670d4dabcbf27
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-06-01 20:28:27 +0000

    feat: ✨ improve plugin install

--- src/githooks/_commit-msg.sh
+++ src/githooks/_commit-msg.sh
@@ -6,7 +6,9 @@ if [ -t 1 ]; then
     exec >/dev/tty 2>&1
 fi
 
+# Install commitizen plugins
+git hook run install-plugins -- '.commitlint.extends//""'
+
 # Apply commitlint rules to the latest commit message
 zz_log i "Applying commitlint rules to the latest commit..."
-PLUGINS=$(cat package.json | npx --yes jqn '.commitlint.extends' | tr -d "'[]:")
-npm list $PLUGINS 2>/dev/null 1>&2 || npm install --no-save $PLUGINS 2>/dev/null 1>&2 && npx --yes commitlint --edit "$1" && npx --yes devmoji -e
+npx --yes commitlint --edit "$1" && npx --yes devmoji -e
