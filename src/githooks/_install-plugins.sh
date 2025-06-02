3c90685cb2d6dacff84c6247a2ae0bcf3d2a7c61
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-06-02 06:53:44 +0000

    fix: 🐛 remove extra logs

--- src/githooks/_install-plugins.sh
+++ src/githooks/_install-plugins.sh
@@ -7,9 +7,7 @@ plugins=$(cat $json_file | npx --yes jqn "$1" | tr -d "'[]:,\"\n")
 
 if [ -z "$plugins" ]; then
     zz_log w "No plugins found at key {B $json_key} in {U $json_file}"
-elif npm list $plugins 2>/dev/null 1>&2; then
-    zz_log s "Plugins {B $plugins} already installed!"
-else
+elif ! npm list $plugins 2>/dev/null 1>&2; then
     zz_log i "Installing plugins {B $plugins} ..."
     npm install --no-save $plugins 2>/dev/null 1>&2
     zz_log s "Plugins {B $plugins} installed successfully!"
