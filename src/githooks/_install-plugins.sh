af4b6b708d5b30f9512d6ee46e397acc4ff68846
Thomas G. <tomgrv@users.perspikapps.fr>
2025-06-02 13:31:24 +0000

    fix: 🐛 check already installed plugins

--- src/githooks/_install-plugins.sh
+++ src/githooks/_install-plugins.sh
@@ -3,11 +3,22 @@
 json_key=$1
 json_file=${2:-package.json}
 
-plugins=$(cat $json_file | npx --yes jqn "$1" | tr -d "'[]:,\"\n")
+plugins=$(cat $json_file | npx --yes jqn "$1" | tr -d "'[]:,\"")
 
 if [ -z "$plugins" ]; then
     zz_log w "No plugins found at key {B $json_key} in {U $json_file}"
 elif ! npm list $plugins 2>/dev/null 1>&2; then
+
+    config=$(dirname $0)/.ci-plugins
+
+    # Save the plugins to a config file
+    zz_log i "Adding plugins {B $plugins} to list of CI dependencies ..."
+    echo "$plugins" | sed 's/^ *//;s/ *$//' | grep -v --file=$config >>$config
+
+    # Reload the plugins list
+    plugins=$(cat $config | tr '\n' ' ')
+
+    # Install the plugins
     zz_log i "Installing plugins {B $plugins} ..."
     npm install --no-save $plugins 2>/dev/null 1>&2
     zz_log s "Plugins {B $plugins} installed successfully!"
