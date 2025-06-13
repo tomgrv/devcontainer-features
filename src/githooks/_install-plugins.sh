4f0686751550325d47c046a5629d1abe30562293
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-06-13 15:25:06 +0000

    fix(githooks): 🐛 change jqn for jq

--- src/githooks/_install-plugins.sh
+++ src/githooks/_install-plugins.sh
@@ -3,7 +3,8 @@
 json_key=$1
 json_file=${2:-./package.json}
 
-plugins=$(cat $json_file | npx --yes jqn "$1" | tr -d "'[]:,\"")
+zz_log i "Using file {B $json_file}..."
+plugins=$(jq -r "$1" "$json_file" | tr -d "'[]:,\"")
 
 if [ -z "$plugins" ]; then
     zz_log w "No plugins found at key {B $json_key} in {U $json_file}"
@@ -20,7 +21,7 @@ for plugin in $plugins; do
         # Save the plugins to a config file
         zz_log i "Adding plugin {B $plugin} to list of CI dependencies ..."
         echo "$plugin" | sed 's/^ *//;s/ *$//' | grep -v --file=$config >>$config
-    fi 
+    fi
 done
 
 # Reload the plugins list
@@ -28,7 +29,7 @@ plugins=$(cat $config | tr '\n' ' ')
 
 # Install the plugins
 zz_log i "Installing plugins {B $plugins} ..."
-if ! npm install --no-save $plugins 2>/dev/null 1>&2 ; then
+if ! npm install --no-save $plugins 2>/dev/null 1>&2; then
     zz_log e "Failed to install one of plugins {B $plugins}!"
     exit 1
 fi
