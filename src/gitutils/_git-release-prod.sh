9de255ed621955a6cdaf3793d563e104cff7e13a
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2025-10-02 11:33:04 +0000

    fix(gitutils): 🐛 refine version extraction

--- src/gitutils/_git-release-prod.sh
+++ src/gitutils/_git-release-prod.sh
@@ -12,14 +12,14 @@ cd "$(git rev-parse --show-toplevel)" >/dev/null
 # Check if on a hotfix branch and extract branch name
 if [ -n "$(git branch --list hotfix/*)" ]; then
     flow=hotfix
-    name=$(git branch --list hotfix/* | sed 's/.*hotfix\///')
+    name=$(git branch --list hotfix/* | sed 's/.*hotfix\///'| head -n1 )
     zz_log i "Hotfix branch found: {Yellow $name}"
 fi
 
 # Check if on a release branch and extract branch name
 if [ -n "$(git branch --list release/*)" ]; then
     flow=release
-    name=$(git branch --list release/* | sed 's/.*release\///')
+    name=$(git branch --list release/* | sed 's/.*release\///' | head -n1 )
     zz_log i "Release branch found: {Blue $name}"
 fi
 
@@ -37,7 +37,7 @@ if [ -z "$flow" ] || [ -z "$name" ]; then
 fi
 
 # Extract branch name 
-if ! git checkout $flow/$name; then
+if ! git checkout $flow/$name >/dev/null 2>&1; then
     zz_log e "Cannot switch to $flow/$name branch"
     exit 1
 fi
@@ -55,7 +55,7 @@ zz_log i "Bump version: {Blue $GBV}"
 GIT_EDITOR=:
 
 # Update version, changelog, and finish release
-if bump-changelog -f $GBV -b -m; then
+if bump-changelog -f $GBV -b; then
     zz_log s "Version & CHANGELOG updated to: {B $GBV}" 
     if ! git commit -am "chore(release): $GBV"; then
         zz_log e "Cannot commit version & CHANGELOG"
