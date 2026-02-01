82ac41ea78a53d1a8637e6cf64d05e1a37362459
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2026-02-01 21:09:45 +0000

    feat: ✨ update GitVersion installation to use Docker and adjust versioning

--- src/gitversion/_gv.sh
+++ src/gitversion/_gv.sh
@@ -2,4 +2,5 @@
 
 # Call the GitVersion tool with the specified configuration
 /usr/local/bin/dotnet-gitversion  $(git rev-parse --show-toplevel)  -config ".gitversion" "$@"
+/usr/local/bin/docker-gitversion  $(git rev-parse --show-toplevel)  -config ".gitversion" "$@"
 
