bedacbd58a8115598908f11dacb47778711f7396
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2026-02-23 18:10:18 +0000

    feat: ✨ improve server script usage instructions and variable consistency

--- src/larasets/_srv.sh
+++ src/larasets/_srv.sh
@@ -4,6 +4,12 @@ set -e
 #### Goto repository root
 cd "$(git rev-parse --show-toplevel)" >/dev/null
 
+#### If no arguments are provided, show usage
+eval $(zz_args "Start or restart the server" $0 "$@" <<-help
+    - name    name        Server name
+help
+)
+
 #### Execute command
 if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ] && sail ps --status running | grep --after-context=1 -q -; then
     server='sail npx --yes pm2'
@@ -11,5 +17,5 @@ else
     server='npx --yes pm2'
 fi
 
-$server restart --update-env server_$1 || $server --name server_$1 start "FORCE_COLOR=1 npm -- run \"$@\""
-$server log --raw --out server_$1
+$server restart --update-env server_$name || $server --name server_$name start "FORCE_COLOR=1 npm -- run \"$@\""
+$server log --raw --out server_$name
