8ff790de6c3615f63fdb457b46e2dcf17ce64550
Thomas G. <1809566+tomgrv@users.noreply.github.com>
2026-03-22 19:49:00 +0000

    fix(larasets): 🐛 improve logging for pm2 execution context

--- src/larasets/_srv.sh
+++ src/larasets/_srv.sh
@@ -12,8 +12,10 @@ help
 
 #### Execute command
 if [ -n "$LARAVEL_SAIL" ] && [ "$LARAVEL_SAIL" -eq 1 ] && sail ps --status running | grep --after-context=1 -q -; then
+    zz_log i "Running pm2 inside Sail"
     server='sail npx --yes pm2'
 else
+    zz_log i "Running pm2 locally"
     server='npx --yes pm2'
 fi
 
